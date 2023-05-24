457fd3bc5eb57b5e41f6a7fa5d63b6b2bb68a4b0
Javier Aburto Galeano <jaburto@gcp.net>
2023-05-24 17:33:17 -0600

    Initial commit

---
+++ app/src/main/java/com/example/welcome/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.welcome;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.content.Intent;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    //
+
+    Button button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        button=(Button)findViewById(R.id.button);
+    }
+
+    public void onClick(View v) {
+        Intent intent = new Intent(MainActivity.this,SecondActivity.class);
+        startActivity(intent);
+    }
+
+}
---
+++ app/src/main/java/com/example/welcome/SecondActivity.java
@@ -0,0 +1,28 @@
+package com.example.welcome;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class SecondActivity extends AppCompatActivity {
+
+    Button button;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+
+        button=(Button)findViewById(R.id.button2);
+    }
+
+
+    public void onClick(View v) {
+        Intent intent = new Intent(SecondActivity.this,MainActivity.class);
+        startActivity(intent);
+    }
+}
