93fb959e0d9a260b24387fa3f4eba2cd152d3a38
jaburtog <jaburto@casapellas.com>
2023-11-02 15:02:25 -0600

    This changes were done by JAAG

--- app/src/main/java/com/example/welcome/SecondActivity.java
+++ app/src/main/java/com/example/welcome/SecondActivity.java
@@ -6,16 +6,22 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class SecondActivity extends AppCompatActivity {
 
+
     Button button;
+
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+
+        //Displaying Toast with Hello Javatpoint message
+        Toast.makeText(getApplicationContext(),"Hello Javatpoint, PROOF",Toast.LENGTH_SHORT).show();
 
         button=(Button)findViewById(R.id.button2);
     }
